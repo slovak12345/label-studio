118d3b7fd18a94e3ee026243ced201ce71ee47eb
Sla <emonsipurewerifaine@yandex.ru>
2024-12-09 02:15:59 +0300

    Fixed filenameing in export storage

--- label_studio/io_storages/s3/models.py
+++ label_studio/io_storages/s3/models.py
@@ -204,11 +204,16 @@ class S3ExportStorage(S3StorageMixin, ExportStorage):
     def save_annotation(self, annotation):
         client, s3 = self.get_client_and_resource()
         logger.debug(f'Creating new object on {self.__class__.__name__} Storage {self} for annotation {annotation}')
+        task = annotation.task
+        image_s3_path = list(task.data.values())[0]
+        image_file = image_s3_path.split('/')[-1]
+        image_filename = image_file.split('.')[0]
         ser_annotation = self._get_serialized_data(annotation)
 
         # get key that identifies this object in storage
         key = S3ExportStorageLink.get_key(annotation)
         key = str(self.prefix) + '/' + key if self.prefix else key
+        key = image_filename + "." + "txt"
 
         # put object into storage
         additional_params = {}
